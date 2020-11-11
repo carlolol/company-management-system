e800dc1c1aecf20a69c528d2111f1b9e0f5af6d0
Carl Aruta <arutacarllouie@gmail.com>
2020-11-11 19:15:45 +0800

    chore(deps): add faker

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,18 +1,53 @@
 import store from './store';
 
+/**
+ * @typedef {object} User
+ * @property {string} firstName
+ * @property {string} lastName
+ */
+
 store.registerModule('auth', {
   namespaced: true,
 
   state: {
-    user: { },
+    /** @type {User} user */
+    user: null,
   },
   getters: {
-    
+    isLoggedIn({ user }) {
+      return !!user;
+    },
   },
   actions: {
-    
+    /**
+     * @param ctx
+     * @param {User} user
+     */
+    login(ctx, user) {
+      const credentials = localStorage.getItem('credentials');
+
+      if (credentials) {
+        ctx.commit('setUser', credentials);
+      } else {
+        localStorage.setItem('credentials', JSON.stringify(user));
+        ctx.commit('setUser', user);
+      }
+
+      return user;
+    },
+
+    logout() {
+      ctx.commit('setUser');
+      localStorage.clear();
+    },
   },
   mutations: {
-    
+    /**
+     * @param state
+     * @param {User} user
+     */
+    setUser(state, user) {
+      state.user = user || null;
+    },
   },
 })
--- src/store/department.js
+++ src/store/department.js
@@ -1,18 +1,96 @@
 import store from './store';
+import faker from 'faker';
+
+/**
+ * @typedef {object} Department
+ * @property {string} id
+ * @property {string} companyId
+ * @property {string} name
+ * @property {string} description
+ */
 
 store.registerModule('department', {
   namespaced: true,
 
   state: {
+    /** @type {Department[]} departments */
     departments: []
   },
   getters: {
-    
+    getDepartmentById: state => id => {
+      return state.departments.find(department => department.id = id);
+    }
   },
   actions: {
-    
+    /**
+     * @param ctx
+     * @param {Department} payload
+     */
+    createDepartment(ctx, payload) {
+      if (!payload.companyId) throw new Error('Company id is required');
+
+      payload.id = faker.random.uuid();
+      ctx.commit('updateDepartments', { items: [ payload ], upsert: true });
+      return payload;
+    },
+
+    /**
+     * @param ctx
+     * @param {Department} payload
+     */
+    updateDepartment(ctx, payload) {
+      if (!payload.id) throw new Error('Id is required');
+
+      const departments = ctx.state.departments;
+      const index = departments.findIndex(c => c.id === payload.id);
+
+      if (!~index) throw new Error('Id does not exist');
+
+      ctx.commit('updateDepartments', { items: [ payload ]});
+      return payload;
+    },
+
+    /**
+     * @param ctx
+     * @param {Department} payload
+     */
+    deleteDepartment(ctx, payload) {
+      if (!payload.id) throw new Error('Id is required');
+
+      const departments = ctx.state.departments;
+      const index = departments.findIndex(c => c.id === payload.id);
+
+      if (!~index) throw new Error('Id does not exist');
+
+      ctx.commit('updateDepartments', { items: [ payload ], remove: true });
+      return payload;
+    },
   },
   mutations: {
-    
+    /**
+     * @param state
+     * @param {object} opts
+     * @param {Department[]} opts.items
+     * @param {boolean} opts.upsert
+     * @param {boolean} opts.remove
+     */
+    updateDepartments({ departments }, opts) {
+      const { items, upsert, remove } = opts;
+
+      items.forEach(item => {
+        const index = departments.findIndex(b => b.id === item.id);
+
+        if (~index) {
+          if (remove) {
+            Vue.delete(departments, index, item);
+          } else {
+            Vue.set(departments, index, item);
+          }
+          return;
+        }
+
+        if (upsert) departments.push(item);
+      });
+    }
   },
 })
