7b8dbc5d82fee89bc087a2b53efeebd9f078d551
Carl Aruta <arutacarllouie@gmail.com>
2020-11-12 12:17:59 +0800

    feat(company): add company data table functionality

--- src/views/Company.vue
+++ src/views/Company.vue
@@ -1,14 +1,154 @@
 <template lang="pug">
-  h1 Company
+  v-container(fluid)
+    v-row(justify="center")
+      v-col(
+        cols="12"
+        sm="10"
+        md="8"
+      )
+        v-data-table(
+          :headers="headers"
+          :items="companies"
+          sort-by="id"
+          class="elevation-1"
+        )
+          template(#top)
+            v-toolbar(
+              flat
+              color="white"
+            )
+              v-spacer
+              v-dialog(
+                v-model="dialog"
+                max-width="500px"
+              )
+                template(#activator="{ on, attrs }")
+                  v-btn(
+                    color="primary"
+                    dark
+                    class="mb-2"
+                    v-bind="attrs"
+                    v-on="on"
+                  ) New Company
+                v-card
+                  v-card-title
+                    span(class="headline") {{ formTitle }}
+                  v-card-text
+                    v-container
+                      v-row
+                        v-col(cols="12")
+                          v-text-field(
+                            v-model="editedItem.name"
+                            label="Name"
+                          )
+                        v-col(cols="12")
+                          v-text-field(
+                            v-model="editedItem.location"
+                            label="Location"
+                          )
+                        v-col(cols="12")
+                          v-textarea(
+                            v-model="editedItem.description"
+                            label="Description"
+                          )
+                  v-card-actions
+                    v-spacer
+                    v-btn(
+                      dark
+                      depressed
+                      color="blue darken-1"
+                      @click="save"
+                    ) Save
+                    v-btn(
+                      dark
+                      text
+                      color="blue darken-1"
+                      @click="close"
+                    ) Close
+
+          template(#item.actions="{ item }")
+            v-icon(
+              small
+              class="mr-2"
+              color="warning"
+              @click="editItem(item)"
+            ) mdi-pencil
+            v-icon(
+              small
+              color="error"
+              @click="deleteItem(item)"
+            ) mdi-delete
+          template(#no-data)
+            span.mx-auto #[i No results found]
 </template>
 
 <script>
 export default {
   name: 'company',
   data: () => ({
-    companies: [],
+    dialog: false,
+    headers: [
+      { text: 'ID', value: 'id' },
+      { text: 'Name', value: 'name' },
+      { text: 'Location', value: 'location' },
+      { text: 'Description', value: 'description' },
+      { text: '', value: 'actions', sortable: false, align: 'end' },
+    ],
+    editedIndex: -1,
+    editedItem: {
+      id: '',
+      name: '',
+      location: '',
+      description: '',
+    },
+    defaultItem: {
+      id: '',
+      name: '',
+      location: '',
+      description: '',
+    },
   }),
-  created () {
-  }
+
+  computed: {
+    formTitle () {
+      return this.editedIndex === -1 ? 'New Company' : 'Edit Company';
+    },
+    companies () {
+      return this.$store.state.company.companies;
+    }
+  },
+
+  watch: {
+    dialog (val) {
+      val || this.close();
+    },
+  },
+
+  methods: {
+    editItem (item) {
+      this.editedIndex = this.companies.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    async deleteItem (item) {
+      confirm('Are you sure you want to delete this company?')
+        && await this.$store.dispatch('company/deleteCompany', item);
+    },
+
+    close () {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    save () {
+      const action = ~this.editedIndex ? 'update' : 'create';
+      this.$store.dispatch(`company/${action}Company`, this.editedItem);
+      this.close();
+    },
+  },
 }
 </script>
